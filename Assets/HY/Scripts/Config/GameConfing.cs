4ac9f55682832fedc2902329da2b274931d22db9
sunsi <155639260+sunsi-game@users.noreply.github.com>
2025-10-22 03:01:14 +0900

    전반적인 게임 구조
    틀 완성

--- Assets/HY/Scripts/Config/GameConfing.cs
+++ Assets/HY/Scripts/Config/GameConfing.cs
@@ -5,6 +5,6 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Configs/GameConfig")]
 public class GameConfing : ScriptableObject {
     [Min(10f)] public float gameDurationSeconds = 600f;
-    public string nextSceneName = "NextStage";
+    public string nextSceneName = "main";
     public KeyCode interactKey = KeyCode.E;
 }
--- Assets/HY/Scripts/Test/SimplePlayer.cs
+++ Assets/HY/Scripts/Test/SimplePlayer.cs
@@ -3,10 +3,21 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D))]
 public class SimplePlayer: MonoBehaviour
 {
+    private static SimplePlayer instance;
+
     public float speed = 5f;
     private Rigidbody2D rb;
     private Vector2 move;
-    void Awake() { rb = GetComponent<Rigidbody2D>(); }
+    void Awake() { 
+        rb = GetComponent<Rigidbody2D>(); 
+        
+        if (instance != null && instance != this) {
+            Destroy(this.gameObject);
+        } else {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+    }
     void Update()
     {
         move = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
