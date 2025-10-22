dd04cd49d59c099c38b11ca6807ecea638dbf712
j1sung <dalssagi00@gmail.com>
2025-10-22 17:45:20 +0900

    refactor: Update DropTable SO

--- Assets/JS/Scripts/Item/DropTable.cs
+++ Assets/JS/Scripts/Item/DropTable.cs
@@ -3,20 +3,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public interface IDropItem
-{
-    string ItemName { get; }
-    Sprite Image { get; }
-}
-
 [System.Serializable]
 public class DropEntry
 {
     //public ItemData_JS item; // 아이템 SO
-    public ScriptableObject item; // 임시 사용
+    public GameObject prefab; // 직접 프리펩 넣기
     public float weight; // 가중치(확률) 설정
-
-    public IDropItem DropItem => item as IDropItem;
 }
 
 [CreateAssetMenu(fileName = "DropTable", menuName = "Item/DropTable")]
