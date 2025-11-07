5e2863c6e25a209238e88e36933d9b17fbfb3e9d
YeongHo <k4h4y1@gmail.com>
2025-11-07 23:33:24 +0900

    fix : timer

--- Assets/HY/Scripts/Services/PauseManager.cs
+++ Assets/HY/Scripts/Services/PauseManager.cs
@@ -4,7 +4,16 @@ using UnityEngine;
 
 public class PauseManager : MonoBehaviour
 {
+    public static PauseManager Instance { get; private set; }
     public bool IsPaused { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this) { Destroy(gameObject); return; }
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+
+    }
 
     public void Pause()
     {
